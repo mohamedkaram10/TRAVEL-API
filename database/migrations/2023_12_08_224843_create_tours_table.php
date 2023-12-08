72fbb53518dbfdff0449c429533ac3e67d36099d
mohamedkaram10 <kokuzzali909.com>
2023-12-09 01:16:28 +0200

    Finish Database and Tables

--- database/migrations/2023_12_08_224843_create_tours_table.php
+++ database/migrations/2023_12_08_224843_create_tours_table.php
@@ -4,19 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tours', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->foreignUuid('travel_id')->constrained('travels');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->date('starting_date');
+            $table->date('ending_date');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class() extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tours');
     }
 };
